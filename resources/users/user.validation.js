0023db1941416089b3c9b5251632e2e9e49a208a
Akere Adetola <adetolaakere1@gmail.com>
2020-02-06 10:39:44 +0100

    (feat): user login

--- resources/users/user.validation.js
+++ resources/users/user.validation.js
@@ -22,5 +22,20 @@ module.exports = {
         .required(),
     };
     return Joi.validate(user, schema);
+  },
+
+  validateLogin(user) {
+    const schema = {
+      email: Joi.string()
+        .min(2)
+        .max(50)
+        .required()
+        .email(),
+      password: Joi.string()
+        .min(5)
+        .max(50)
+        .required(),
+    };
+    return Joi.validate(user, schema);
   }
 };
--- resources/users/userController.js
+++ resources/users/userController.js
@@ -35,20 +35,55 @@ const register = async (req,res) =>{
     })
     await doc.save();
 
-    res.status(201).json({
+   return res.status(201).json({
         status:201,
         message:'User created successfully',
         user:AuthHelper.Auth.toAuthJSON(doc)
     });
+    
     }
 catch(error){
     return res.status(500).json({
         status: 500,
-        error,
+        error:error.message || 'Something went wrong'
     }
     )}
 
-
 }
 
-module.exports = {register}
+const login = async (req,res) => {
+    try{
+        const {error} =validation.validateLogin(req.body)
+        if(error){
+            res.status(422).json({
+                status:422,
+                error:error.details[0].message
+            })
+        }
+            const { email, password} = req.body
+
+            const existingUser = await Users.findOne({email})
+            if(!existingUser){
+                res.status(400).json({
+                    status:400,
+                    message:'Invalid email or password'
+                })
+            }
+            const userPassword = await bcrypt.compareSync(password,existingUser.password)
+            if(!userPassword){
+                res.status(400).json({
+                    message:'invalid email or password'
+                })
+            }
+            return res.status(200).json({
+                message:'Logged in successfully',
+                user:AuthHelper.Auth.toAuthJSON(existingUser)
+            })
+    }
+    catch(error){
+        return res.status(500).json({
+            message: error.message || 'Could not login user',
+          });
+    }
+}
+module.exports = {register,login}
