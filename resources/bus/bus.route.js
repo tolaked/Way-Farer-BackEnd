b3d2043d3cec32269bf3aa03810462d26146e80a
Akere Adetola <adetolaakere1@gmail.com>
2020-02-08 01:00:13 +0100

    (chore): configured eslint protect admin only routes

--- resources/bus/bus.route.js
+++ resources/bus/bus.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const {verifyToken} = require('../../utils/validateToken')
 const router = express.Router()
 
 const { addBus,getAllBuses } = require('./busController');
 
 router.post('/addbus',addBus);
-router.get('/',getAllBuses)
+router.get('/',verifyToken,getAllBuses)
 
 module.exports = router                                                                                                                                                                                                                                                                                                                                                                              
---
+++ utils/validateToken.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization;
+    if(!token){
+        return res.status(401).json({ message:'unauthorized, please log in' });
+    }
+
+    try{
+    jwt.verify(token, process.env.SECRET_KEY, (error, decodedToken) => {
+      if (error) {
+       return res.status(401).json({
+          message: 'invalid token provided'
+        });
+      } 
+        req.decodedToken = decodedToken;
+        
+        if(!decodedToken.isAdmin){
+            return res.status(403).json({
+                message:'admin only route'
+            })
+        }
+         return next();
+    });
+
+}
+catch(error){
+    return res.status(500).json({
+        error:'Something went wrong'
+    })
+}
+};
+
+module.exports = { verifyToken }
