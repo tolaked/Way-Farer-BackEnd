3ee80bbcd1dee21893ca7403b9a8dcf2fce219eb
Akere Adetola <adetolaakere1@gmail.com>
2020-02-17 23:10:50 +0100

    get all-bookings

--- resources/bookings/booking.route.js
+++ resources/bookings/booking.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const { verifyToken } = require('../../utils/validateToken');
-const { bookSeat } = require('./booking.controller');
+const { bookSeat, getAllBookings } = require('./booking.controller');
 
 const router = express.Router();
 
 router.post('/create/:tripId', verifyToken, bookSeat);
+router.get('/', verifyToken, getAllBookings);
 
 module.exports = router;
